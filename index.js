d7b7cb522f4f97469bb51ed8f327d6ae5602febb
Rhinoroo <73572438+AdrianMEvans@users.noreply.github.com>
2020-12-14 00:38:09 +0800

    adding js to change colors and add to local storage

--- index.js
+++ index.js
@@ -4,10 +4,7 @@ const clock = document.querySelector(".clock");
 
 const tick = () => {
     const now = new Date();
-    
-    console.log(now);
-
-    // console.log(dateFns.format(now, 'MMMM'))
+    // console.log(now);
     const h = dateFns.format(now, 'HH');
     const m = dateFns.format(now, 'mm');
     const s = dateFns.format(now, 'ss');
@@ -22,38 +19,187 @@ const tick = () => {
     <span>Time: ${h}:</span><span>${m}:</span><span>${s}</span>
     `;
     // console.log(h,m,s)
-    
+
     clock.innerHTML = html;
-};
-
-setInterval(tick, 1000);
-
-const times = [08, 09, 10, 11, 12, 13, 14, 15, 16]
-
-for (let i = 0; i < times.length; i++) {
-    
-    console.log(times[i]);
-    let rowHtml = `
-    <div class="row">
-    <div class="col hour time-block" id="time${times[i]}">
-    ${times[i]}
-    </div>
-    <div class="col text-block">
-    <!-- text input -->
-    <textarea name="" id="text${times[i]} "cols="30" rows="0" placeholder="enter text here"></textarea>
-    </div>
-    <button class="col saveBtn btn-block" i:hover id="btn${times[i]}"><i class="far fa-save"></i></button>
-    </div>
-    </div>`
-    
-    console.log(rowHtml);
-
-    let newRows = document.querySelector(".rowContainer");
-    newRows.innerHTML += rowHtml;
 }
 
-console.log('loop finished');
+setInterval(tick, 1000);
+const now = new Date();
 
-// create a variable consisting of an array with the hours for the scheduler 
-// create row with 3 columns
-// give each colum an id referencing the time for that row 
+const h = dateFns.format(now, 'HH');
+const m = dateFns.format(now, 'mm');
+const s = dateFns.format(now, 'ss');
+
+const times = ['08', '09', '10', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '22'];
+
+for (let i = 0; i < times.length; i++) {
+    const time = times[i];
+    // console.log(time);
+    // console.log(times[i]);
+    let rowHtml = `
+    <div class="row">
+        <div class="col hour time-block" id="time${time}">
+        ${time} : 00
+        </div>
+        <div class="col text-block">
+            <textarea name="${time}" id="${time}" "cols="30" rows="0" placeholder="enter text here"></textarea>
+        </div>
+        <button class="col saveBtn btn-block" i:hover id="btn${time}"><i class="far fa-save"></i></button>  
+    </div>`
+
+    // console.log(rowHtml);
+    let newRows = document.querySelector(".rowContainer");
+    newRows.innerHTML += rowHtml;
+
+    const myRow = document.querySelectorAll(".row");
+    // console.log(myRow);
+
+    // console.log(time);
+}
+
+let textAreaID = document.querySelectorAll("textarea")
+// console.log(textAreaID);
+
+// console.log(h);
+
+const textDiv08 = document.getElementById("08");
+const textDivAttribute08 = textDiv08.getAttribute("id")
+// console.log(textDivAttribute08);
+
+if (textDivAttribute08 == h) {
+    textDiv08.parentElement.classList.add("present")
+} else if (textDivAttribute08 > h) {
+    textDiv08.parentElement.classList.add("future")
+} else if (textDivAttribute08 < h) {
+    textDiv08.parentElement.classList.add("past")
+}
+
+const textDiv09 = document.getElementById("09");
+const textDivAttribute09 = textDiv09.getAttribute("id")
+// console.log(textDivAttribute09);
+
+if (textDivAttribute09 == h) {
+    textDiv09.parentElement.classList.add("present")
+} else if (textDivAttribute09 > h) {
+    textDiv09.parentElement.classList.add("future")
+} else if (textDivAttribute09 < h) {
+    textDiv09.parentElement.classList.add("past")
+}
+
+const textDiv10 = document.getElementById("10");
+const textDivAttribute10 = textDiv10.getAttribute("id")
+
+if (textDivAttribute10 == h) {
+    textDiv10.parentElement.classList.add("present")
+} else if (textDivAttribute10 > h) {
+    textDiv10.parentElement.classList.add("future")
+} else if (textDivAttribute10 < h) {
+    textDiv10.parentElement.classList.add("past")
+}
+
+const textDiv11 = document.getElementById("11");
+const textDivAttribute11 = textDiv11.getAttribute("id")
+
+if (textDivAttribute11 == h) {
+    textDiv11.parentElement.classList.add("present")
+} else if (textDivAttribute11 > h) {
+    textDiv11.parentElement.classList.add("future")
+} else if (textDivAttribute11 < h) {
+    textDiv11.parentElement.classList.add("past")
+}
+
+const textDiv12 = document.getElementById("12");
+const textDivAttribute12 = textDiv12.getAttribute("id")
+
+if (textDivAttribute12 == h) {
+    textDiv12.parentElement.classList.add("present")
+} else if (textDivAttribute12 > h) {
+    textDiv12.parentElement.classList.add("future")
+} else if (textDivAttribute12 < h) {
+    textDiv12.parentElement.classList.add("past")
+}
+
+const textDiv13 = document.getElementById("13");
+const textDivAttribute13 = textDiv13.getAttribute("id")
+
+if (textDivAttribute13 == h) {
+    textDiv13.parentElement.classList.add("present")
+} else if (textDivAttribute13 > h) {
+    textDiv13.parentElement.classList.add("future")
+} else if (textDivAttribute13 < h) {
+    textDiv13.parentElement.classList.add("past")
+}
+
+const textDiv14 = document.getElementById("14");
+const textDivAttribute14 = textDiv14.getAttribute("id")
+
+if (textDivAttribute14 == h) {
+    textDiv14.parentElement.classList.add("present")
+} else if (textDivAttribute14 > h) {
+    textDiv14.parentElement.classList.add("future")
+} else if (textDivAttribute14 < h) {
+    textDiv14.parentElement.classList.add("past")
+}
+
+const textDiv15 = document.getElementById("15");
+const textDivAttribute15 = textDiv15.getAttribute("id")
+
+if (textDivAttribute15 == h) {
+    textDiv15.parentElement.classList.add("present")
+} else if (textDivAttribute15 > h) {
+    textDiv15.parentElement.classList.add("future")
+} else if (textDivAttribute15 < h) {
+    textDiv15.parentElement.classList.add("past")
+}
+
+const textDiv16 = document.getElementById("16");
+const textDivAttribute16 = textDiv16.getAttribute("id")
+
+if (textDivAttribute16 == h) {
+    textDiv16.parentElement.classList.add("present")
+} else if (textDivAttribute16 > h) {
+    textDiv16.parentElement.classList.add("future")
+} else if (textDivAttribute16 < h) {
+    textDiv16.parentElement.classList.add("past")
+}
+
+const textDiv17 = document.getElementById("17");
+const textDivAttribute17 = textDiv17.getAttribute("id")
+
+if (textDivAttribute17 == h) {
+    textDiv17.parentElement.classList.add("present")
+} else if (textDivAttribute17 > h) {
+    textDiv17.parentElement.classList.add("future")
+} else if (textDivAttribute17 < h) {
+    textDiv17.parentElement.classList.add("past")
+}
+
+const textDiv20 = document.getElementById("20");
+const textDivAttribute20 = textDiv20.getAttribute("id")
+
+if (textDivAttribute20 == h) {
+    textDiv20.parentElement.classList.add("present")
+} else if (textDivAttribute20 > h) {
+    textDiv20.parentElement.classList.add("future")
+} else if (textDivAttribute20 < h) {
+    textDiv20.parentElement.classList.add("past")
+}
+
+const textDiv22 = document.getElementById("22");
+const textDivAttribute22 = textDiv22.getAttribute("id")
+
+if (textDivAttribute22 == h) {
+    textDiv22.parentElement.classList.add("present")
+} else if (textDivAttribute22 > h) {
+    textDiv22.parentElement.classList.add("future")
+} else if (textDivAttribute22 < h) {
+    textDiv22.parentElement.classList.add("past")
+}
+
+$('.saveBtn').click(function () {
+    const time = $(this).siblings('div.time-block').text();
+    // console.log(time);
+    const input = $(this).siblings('div.text-block').children("textarea").val();
+    // console.log(input);
+    localStorage.setItem(time, input);
+ })
